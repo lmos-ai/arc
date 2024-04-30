252596f0b5baa0329d4c4fad0e627480f8bac902
patrick.whelan <patrick.whelan@telekom.de>
2024-04-30 22:14:43 +0200

    doc: add init page

---
+++ docs/assets/js/5e95c892.46159e0b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkarc=self.webpackChunkarc||[]).push([[647],{7121:(e,r,s)=>{s.r(r),s.d(r,{default:()=>d});s(6540);var a=s(4164),c=s(1213),u=s(7559),n=s(2831),t=s(781),l=s(4848);function d(e){return(0,l.jsx)(c.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,l.jsx)(t.A,{children:(0,n.v)(e.route.routes)})})}}}]);
