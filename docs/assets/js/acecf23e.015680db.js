73c4745e78ea6784d40789f57b3ae575c52fc988
patrick.whelan <patrick.whelan@telekom.de>
2024-04-30 22:21:45 +0200

    doc: add init page

--- docs/assets/js/acecf23e.015680db.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkarc=self.webpackChunkarc||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog"}')}}]);
---
+++ docs/assets/js/d0d72847.d60b49e2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkarc=self.webpackChunkarc||[]).push([[149],{7671:e=>{e.exports=JSON.parse('{"permalink":"/arc/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
