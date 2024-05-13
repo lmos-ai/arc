178ce6ad2fcf94be2d812464e17df2a1592a8465
patrick.whelan <patrick.whelan@telekom.de>
2024-05-13 09:03:28 +0200

    doc: update docs

--- docs/assets/js/8118daa8.d33277c3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkarc=self.webpackChunkarc||[]).push([[471],{2945:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- docs/assets/js/d0d72847.d60b49e2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkarc=self.webpackChunkarc||[]).push([[149],{7671:e=>{e.exports=JSON.parse('{"permalink":"/arc/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
