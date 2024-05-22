3af35c7cb1b44e32ce6ad47c9875651894d3b528
patrick.whelan <patrick.whelan@telekom.de>
2024-05-22 18:51:44 +0200

    feat: add SemanticRouter to spring boot starter

--- arc-agents/src/main/kotlin/llm/Simularity.kt
+++ arc-agents/src/main/kotlin/llm/Simularity.kt
@@ -1,7 +1,15 @@
+// SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package io.github.lmos.arc.agents.llm
 
 import kotlin.math.sqrt
 
+/**
+ * Calculates the cosine similarity between two vectors.
+ * The result will range from -1.0 to 1.0. With 1.0 meaning the vectors are practically identical.
+ */
 fun similarity(vec1: List<Double>, vec2: List<Double>): Double {
     require(vec1.size == vec2.size) { "Vector sizes must be equal!" }
     val dotProduct = vec1.zip(vec2) { a, b -> a * b }.sum()
