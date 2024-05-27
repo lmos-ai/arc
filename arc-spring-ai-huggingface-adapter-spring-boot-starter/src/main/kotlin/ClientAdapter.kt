ac3ba2bd00ab4779c3774cad8d056c575cd454bc
Maximilian Erren <Maximilian.Erren@telekom.de>
2024-05-27 15:14:53 +0200

    initial implementation of HuggingfaceClientAdapter

---
+++ arc-spring-ai-huggingface-adapter-spring-boot-starter/src/main/kotlin/ClientAdapter.kt
@@ -0,0 +1,65 @@
+// SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import io.github.lmos.arc.agents.ArcException
+import io.github.lmos.arc.agents.conversation.AssistantMessage
+import io.github.lmos.arc.agents.conversation.ConversationMessage
+import io.github.lmos.arc.agents.conversation.SystemMessage
+import io.github.lmos.arc.agents.conversation.UserMessage
+import io.github.lmos.arc.agents.functions.LLMFunction
+import io.github.lmos.arc.agents.llm.ChatCompleter
+import io.github.lmos.arc.agents.llm.ChatCompletionSettings
+import io.github.lmos.arc.core.failWith
+import io.github.lmos.arc.core.result
+import org.springframework.ai.chat.messages.ChatMessage
+import org.springframework.ai.chat.messages.Message
+import org.springframework.ai.chat.messages.MessageType
+import org.springframework.ai.chat.prompt.ChatOptions
+import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.huggingface.HuggingfaceChatClient
+
+private fun messageConversion(msg: ConversationMessage): Message {
+    val role = when (msg) {
+        is UserMessage -> MessageType.USER
+        is AssistantMessage -> MessageType.ASSISTANT
+        is SystemMessage -> MessageType.SYSTEM
+    }
+    return ChatMessage(role, msg.content)
+}
+
+private class SpringAiChatOptionsAdapter(settings: ChatCompletionSettings) : ChatOptions {
+    val settings = settings
+    override fun getTemperature(): Float {
+        return settings.temperature!!.toFloat()
+    }
+
+    override fun getTopP(): Float {
+        return settings.topP!!.toFloat()
+    }
+
+    override fun getTopK(): Int {
+        throw UnsupportedOperationException("getTopK Not supported yet.")
+    }
+}
+
+class HuggingFaceChatClientAdapter(private val springClient: HuggingfaceChatClient) : ChatCompleter {
+    override suspend fun complete(
+        messages: List<ConversationMessage>,
+        functions: List<LLMFunction>?,
+        settings: ChatCompletionSettings?,
+    ) = result<AssistantMessage, ArcException> {
+        val conversationMessages = messages.map { messageConversion(it) }
+        val response = springClient.call(
+            Prompt(
+                conversationMessages,
+                settings?.let { SpringAiChatOptionsAdapter(settings) },
+            ),
+        )
+        response?.result?.output?.content?.let {
+            return@result AssistantMessage(it)
+        } ?: failWith {
+            ArcException("Something went wrong while calling the HuggingFaceChatClient")
+        }
+    }
+}
