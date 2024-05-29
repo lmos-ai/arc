b0caafec5c8bace138f15e37e7eee8b116592789
patrick.whelan <patrick.whelan@telekom.de>
2024-05-29 21:02:56 +0200

    feat: improve metrics.

--- arc-spring-boot-starter/src/main/kotlin/MetricsHandler.kt
+++ arc-spring-boot-starter/src/main/kotlin/MetricsHandler.kt
@@ -10,6 +10,10 @@ import io.github.lmos.arc.agents.llm.LLMFinishedEvent
 import io.github.lmos.arc.agents.router.RouterRoutedEvent
 import io.github.lmos.arc.core.Success
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tag
+import io.micrometer.core.instrument.Timer
+import java.math.RoundingMode.DOWN
+import kotlin.time.Duration
 import kotlin.time.toJavaDuration
 
 /**
@@ -19,41 +23,48 @@ class MetricsHandler(private val metrics: MeterRegistry) : EventHandler<Event> {
 
     override fun onEvent(event: Event) {
         when (event) {
-            is AgentFinishedEvent -> {
+            is AgentFinishedEvent -> with(event) {
                 if (event.output is Success) {
-                    metrics.timer(
+                    timer(
                         "arc.agent.finished",
-                        "agent",
-                        event.agent.name,
-                        "model",
-                        event.model ?: "default",
-                    ).record(event.duration.toJavaDuration())
+                        duration,
+                        tags = mapOf("agent" to agent.name, "model" to (model ?: "default"))
+                    )
                 } else {
-                    metrics.timer(
+                    timer(
                         "arc.agent.failed",
-                        "agent",
-                        event.agent.name,
-                    ).record(event.duration.toJavaDuration())
+                        duration,
+                        tags = mapOf("agent" to agent.name)
+                    )
                 }
             }
 
-            is LLMFinishedEvent -> {
-                metrics.timer(
+            is LLMFinishedEvent -> with(event) {
+                timer(
                     "arc.llm.finished",
-                    "model",
-                    event.model,
-                ).record(event.duration.toJavaDuration())
+                    duration,
+                    tags = mapOf("model" to model)
+                )
             }
 
-            is RouterRoutedEvent -> {
-                metrics.timer(
+            is RouterRoutedEvent -> with(event) {
+                val accuracy = destination?.accuracy?.toBigDecimal()?.setScale(1, DOWN)?.toString() ?: "-1"
+                val destination = destination?.destination ?: "null"
+                timer(
                     "arc.router.routed",
-                    "accuracy",
-                    event.destination?.accuracy?.toString() ?: "-1",
-                    "destination",
-                    event.destination?.destination ?: "null",
-                ).record(event.duration.toJavaDuration())
+                    duration,
+                    tags = mapOf("accuracy" to accuracy, "destination" to destination)
+                )
             }
         }
     }
+
+    private fun timer(name: String, duration: Duration, tags: Map<String, String>) = Timer.builder(name)
+        .tags(tags.map { (k, v) -> Tag.of(k, v) })
+        .distributionStatisticExpiry(java.time.Duration.ofMinutes(5))
+        .distributionStatisticBufferLength(50)  //limit memory usage
+        .publishPercentiles(0.5, 0.95)
+        .percentilePrecision(2)
+        .register(metrics)
+        .record(duration.toJavaDuration())
 }
