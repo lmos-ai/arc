b70184a8899c19f641638d59a2b04abb8d476aea
patrick.whelan <patrick.whelan@telekom.de>
2024-07-02 10:47:59 +0200

    chore: fix formatting

--- arc-spring-ai/src/main/kotlin/Extensions.kt
+++ arc-spring-ai/src/main/kotlin/Extensions.kt
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+//
+// SPDX-License-Identifier: Apache-2.0
 package io.github.lmos.arc.spring.ai
 
 import io.github.lmos.arc.agents.conversation.AssistantMessage
