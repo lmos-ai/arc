087ab26bfc8a7b70ae0b29e9c535d6f0d73bd26e
patrick.whelan <patrick.whelan@telekom.de>
2024-09-12 21:05:46 +0200

    chore(graphql): update dependencies.

--- arc-graphql-spring-boot-starter/src/test/kotlin/TestApplication.kt
+++ arc-graphql-spring-boot-starter/src/test/kotlin/TestApplication.kt
@@ -4,7 +4,33 @@
 
 package ai.ancf.lmos.arc.graphql
 
+import ai.ancf.lmos.arc.agents.ArcException
+import ai.ancf.lmos.arc.agents.conversation.AssistantMessage
+import ai.ancf.lmos.arc.agents.conversation.ConversationMessage
+import ai.ancf.lmos.arc.agents.functions.LLMFunction
+import ai.ancf.lmos.arc.agents.llm.ChatCompleter
+import ai.ancf.lmos.arc.agents.llm.ChatCompleterProvider
+import ai.ancf.lmos.arc.agents.llm.ChatCompletionSettings
+import ai.ancf.lmos.arc.core.result
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-open class TestApplication
+open class TestApplication{
+
+    @Bean
+    open fun chatCompleterProvider() = ChatCompleterProvider {
+        object : ChatCompleter {
+            override suspend fun complete(
+                messages: List<ConversationMessage>,
+                functions: List<LLMFunction>?,
+                settings: ChatCompletionSettings?,
+            ) = result<AssistantMessage, ArcException> {
+                functions?.forEach { function ->
+                    function.execute(mapOf("param" to "test"))
+                }
+                AssistantMessage("Hello!")
+            }
+        }
+    }
+}
