e11885933a9c76d9d2003bb611263b7b031c441b
patrick.whelan <patrick.whelan@telekom.de>
2024-10-28 08:13:35 +0100

    fix: fix formatting

--- arc-spring-ai/src/test/kotlin/SpringChatClientTest.kt
+++ arc-spring-ai/src/test/kotlin/SpringChatClientTest.kt
@@ -4,16 +4,11 @@
 
 package ai.ancf.lmos.arc.spring.ai
 
-import ai.ancf.lmos.arc.agents.llm.complete
-import ai.ancf.lmos.arc.core.getOrNull
-import kotlinx.coroutines.runBlocking
-import org.assertj.core.api.Assertions.assertThat
-
 class SpringChatClientTest : TestBase() {
 
     // @Test
-    fun `test chat completion text`(): Unit = runBlocking {
-        val result = springChatClient.complete("test question")
-        assertThat(result.getOrNull()?.content).isEqualTo("answer to test")
-    }
+    // fun `test chat completion text`(): Unit = runBlocking {
+    //    val result = springChatClient.complete("test question")
+    //    assertThat(result.getOrNull()?.content).isEqualTo("answer to test")
+    // }
 }
