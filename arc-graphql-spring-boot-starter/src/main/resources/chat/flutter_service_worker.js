3054df9f198cb173856a4756d8278902ba47168b
patrick.whelan <patrick.whelan@telekom.de>
2024-11-03 15:18:37 +0100

    fix: fix broken button in arc view

--- arc-graphql-spring-boot-starter/src/main/resources/chat/flutter_service_worker.js
+++ arc-graphql-spring-boot-starter/src/main/resources/chat/flutter_service_worker.js
@@ -3,17 +3,17 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0745c4c4f8b214df91a34405bf0eca6f",
+const RESOURCES = {"flutter_bootstrap.js": "f96935bdeec4510aa977a960426b9d68",
 "version.json": "345101093b11dd469d12df805f974850",
 "index.html": "9ccf7560708fed4e215283023d6517e1",
 "/": "9ccf7560708fed4e215283023d6517e1",
-"main.dart.js": "573f05295dabbcd09b9932c36b78cddd",
+"main.dart.js": "467325bce2a00df866460020c99b4c3b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "cac303fb8820c8845613ead98edf9047",
 "main.dart.mjs": "eac010c284d40c8d5536d71a89f5e266",
 "icons/Icon-192.png": "2e6d3f26571285124673949194cf64a8",
 "manifest.json": "c731c53a201f22d450dfa9d8b7e6a90f",
-"main.dart.wasm": "f571fef81974341c706620fd594c4cf7",
+"main.dart.wasm": "506494a3abb03cb57af603f68fcb5f79",
 "assets/LICENSES/Apache-2.0.txt": "c846ebb396f8b174b10ded4771514fcc",
 "assets/AssetManifest.json": "b502dfd97a38a11da958f5a429265ad0",
 "assets/NOTICES": "ac76f7cebb72ef35772e4cee6ef41e52",
