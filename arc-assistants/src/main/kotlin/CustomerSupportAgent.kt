99a6a6c549a2a5724ca9cc3f4f00595467b9017c
patrick.whelan <patrick.whelan@telekom.de>
2024-11-04 12:27:49 +0100

    fix: fix copyright

--- arc-assistants/src/main/kotlin/CustomerSupportAgent.kt
+++ arc-assistants/src/main/kotlin/CustomerSupportAgent.kt
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+//
+// SPDX-License-Identifier: Apache-2.0
 package ai.ancf.lmos.arc.assistants.support
 
 import ai.ancf.lmos.arc.agents.dsl.AgentDefinitionContext
