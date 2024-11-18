d4096404ddb5bf5f151e014d8bff5ad4b712b7c2
patrick.whelan <patrick.whelan@telekom.de>
2024-11-18 22:31:05 +0100

    chore(docs): update docs

---
+++ docs-src/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/A1214361/projects/ai/public/arc/docs-src/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/A1214361/projects/ai/public/arc/docs-src/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/A1214361/projects/ai/public/arc/docs-src/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/A1214361/projects/ai/public/arc/docs-src/src/css/custom.css"),
+];
