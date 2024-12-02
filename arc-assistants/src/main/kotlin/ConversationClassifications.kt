bbfdeb519ce9ae736b93fb1fc76976a5c9d31fba
patrick.whelan <patrick.whelan@telekom.de>
2024-12-02 16:22:25 +0100

    feat: add AgentHandover ConversationClassification

--- arc-assistants/src/main/kotlin/ConversationClassifications.kt
+++ arc-assistants/src/main/kotlin/ConversationClassifications.kt
@@ -6,6 +6,18 @@ package ai.ancf.lmos.arc.assistants.support
 
 import ai.ancf.lmos.arc.agents.conversation.ConversationClassification
 
+/**
+ * A classification to indicate that the agent was unavailable to resolve the user's request.
+ */
 object Unresolved : ConversationClassification {
     override fun toString() = "UNRESOLVED"
+}
+
+/**
+ * A classification to indicate that the agent has handed over the conversation to a human agent.
+ * This can be because the user explicitly asked for a human agent or because the agent detected
+ * that it cannot resolve the user's request.
+ */
+object AgentHandover : ConversationClassification {
+    override fun toString() = "AGENT_HANDOVER"
 }
