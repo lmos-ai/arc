9bb8bad0b69521ce698662207c4e9c118b001690
patrick.whelan <patrick.whelan@telekom.de>
2024-12-03 21:38:28 +0100

    chore(kotlin): update kotlin to 2.1.0

--- arc-ollama-client/build.gradle.kts
+++ arc-ollama-client/build.gradle.kts
@@ -8,17 +8,17 @@ dependencies {
 
     // Logging
     implementation("org.slf4j:slf4j-api:1.7.25")
-    implementation("io.ktor:ktor-client-cio-jvm:2.3.10")
 
     // ktor
-    val ktorVersion = "2.3.10"
+    val ktorVersion = "2.3.13"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio-jvm:$ktorVersion")
 
     // Ktor Server for tests
-    testImplementation("io.ktor:ktor-server-core-jvm:2.3.11")
-    testImplementation("io.ktor:ktor-server-netty-jvm:2.3.11")
+    testImplementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
 }
