f2be924156ad62e8959ffba742a374f20f3be289
patrick.whelan <patrick.whelan@telekom.de>
2024-12-04 15:13:16 +0100

    chore(view): update arc view

--- arc-graphql-spring-boot-starter/src/main/resources/chat/flutter_bootstrap.js
+++ arc-graphql-spring-boot-starter/src/main/resources/chat/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "148919543"
+    serviceWorkerVersion: "2837013461"
   }
 });
