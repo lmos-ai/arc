0613f49e252185afa9847f4cb23f5b7dfbb3bda1
Jasbir <jasbirsinghkamboj@gmail.com>
2024-12-27 15:19:46 +0530

    init block within arc DSL (#73)

--- arc-agents/src/test/kotlin/ChatAgentTest.kt
+++ arc-agents/src/test/kotlin/ChatAgentTest.kt
@@ -4,9 +4,15 @@
 
 package ai.ancf.lmos.arc.agents
 
-import ai.ancf.lmos.arc.agents.conversation.toConversation
+import ai.ancf.lmos.arc.agents.conversation.*
 import ai.ancf.lmos.arc.agents.dsl.AllTools
 import ai.ancf.lmos.arc.agents.dsl.extensions.breakWith
+import ai.ancf.lmos.arc.agents.functions.LLMFunction
+import ai.ancf.lmos.arc.agents.llm.ChatCompleter
+import ai.ancf.lmos.arc.agents.llm.ChatCompleterProvider
+import ai.ancf.lmos.arc.agents.llm.ChatCompletionSettings
+import ai.ancf.lmos.arc.core.Result
+import ai.ancf.lmos.arc.core.Success
 import ai.ancf.lmos.arc.core.getOrThrow
 import io.mockk.coEvery
 import io.mockk.verify
@@ -84,5 +90,41 @@ class ChatAgentTest : TestBase() {
 
         executeAgent(agent, "question?")
         verify { functionProvider.provideAll() }
+    }
+
+    @Test
+    fun `test init block execution`(): Unit = runBlocking {
+        var initExecuted = 0
+        val agent = agent {
+            name = "agent"
+            description = "agent description"
+            systemPrompt = { "$initExecuted" }
+            init = {
+                initExecuted += 1
+            }
+        } as ChatAgent
+
+        assertThat(initExecuted).isEqualTo(1)
+        var result = testBeanProvider.setContext(setOf(ChatCompleterProvider { TestChatCompleter() })) {
+            agent.execute("test".toConversation(User("user"))).getOrThrow()
+        }
+        assertThat(initExecuted.toString()).isEqualTo(result.transcript.findLast { it is AssistantMessage }?.content)
+
+        result = testBeanProvider.setContext(setOf(ChatCompleterProvider { TestChatCompleter() })) {
+            agent.execute("test".toConversation(User("user"))).getOrThrow()
+        }
+        assertThat(initExecuted.toString()).isEqualTo(result.transcript.findLast { it is AssistantMessage }?.content)
+    }
+
+    class TestChatCompleter : ChatCompleter {
+        override suspend fun complete(
+            messages: List<ConversationMessage>,
+            functions: List<LLMFunction>?,
+            settings: ChatCompletionSettings?,
+        ): Result<AssistantMessage, ArcException> {
+            return messages.findLast { it is SystemMessage }?.let {
+                Success(AssistantMessage(it.content))
+            } ?: throw ArcException("No system message found")
+        }
     }
 }
--- arc-scripting/src/test/kotlin/AgentTest.kt
+++ arc-scripting/src/test/kotlin/AgentTest.kt
@@ -4,11 +4,15 @@
 
 package ai.ancf.lmos.arc.scripting
 
+import ai.ancf.lmos.arc.agents.ArcException
 import ai.ancf.lmos.arc.agents.ChatAgent
 import ai.ancf.lmos.arc.agents.User
-import ai.ancf.lmos.arc.agents.conversation.AssistantMessage
-import ai.ancf.lmos.arc.agents.conversation.ConversationMessage
-import ai.ancf.lmos.arc.agents.conversation.toConversation
+import ai.ancf.lmos.arc.agents.conversation.*
+import ai.ancf.lmos.arc.agents.functions.LLMFunction
+import ai.ancf.lmos.arc.agents.llm.ChatCompleter
+import ai.ancf.lmos.arc.agents.llm.ChatCompleterProvider
+import ai.ancf.lmos.arc.agents.llm.ChatCompletionSettings
+import ai.ancf.lmos.arc.core.Result
 import ai.ancf.lmos.arc.core.Success
 import ai.ancf.lmos.arc.core.getOrThrow
 import io.mockk.coEvery
@@ -36,5 +40,37 @@ class AgentTest : TestBase() {
         }
 
         assertThat(input.captured.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `test agent execution with one time loading from init block`(): Unit = runBlocking {
+        val assistantMessage: String
+        assistantMessage = testBeanProvider.setContext(
+            setOf(
+                ChatCompleterProvider {
+                    object : ChatCompleter {
+                        override suspend fun complete(
+                            messages: List<ConversationMessage>,
+                            functions: List<LLMFunction>?,
+                            settings: ChatCompletionSettings?,
+                        ): Result<AssistantMessage, ArcException> {
+                            return messages.findLast { it is SystemMessage }?.let {
+                                Success(AssistantMessage(it.content))
+                            } ?: throw ArcException("No system message found")
+                        }
+                    }
+                },
+            ),
+        ) {
+            val agent = eval("kb-summarizer.agent.kts") as ChatAgent
+            // execute twice.
+            agent.execute("A question".toConversation(User("test")))
+                .getOrThrow().transcript.findLast { it is AssistantMessage }?.content ?: "No assistant message found"
+            agent.execute("A question".toConversation(User("test")))
+                .getOrThrow().transcript.findLast { it is AssistantMessage }?.content ?: "No assistant message found"
+        }
+
+        assertThat(assistantMessage).contains("This is a file to test init block of arc agents.")
+        assertThat(assistantMessage).contains("Number of times file loaded = 1")
     }
 }
