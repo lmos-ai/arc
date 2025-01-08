bddf45db66e8f0908452d91de48ad93938bda1c9
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-01-08 11:35:01 +0100

    feat: enable usecase requests

--- arc-assistants/src/main/kotlin/extensions/LoadUseCases.kt
+++ arc-assistants/src/main/kotlin/extensions/LoadUseCases.kt
@@ -15,7 +15,10 @@ private val log = LoggerFactory.getLogger("UseCasesLoader")
  * Loads the use case file with the given name.
  */
 suspend fun DSLContext.useCases(name: String, fallbackLimit: Int = 2): String {
-    val useCases = local(name)?.toUseCases() ?: kotlin.error("No use case file found with the name $name!")
+    val requestUseCase = system("usecase", defaultValue = "").takeIf { it.isNotEmpty() }
+    val useCases =
+        (requestUseCase ?: local(name))?.toUseCases() ?: kotlin.error("No use case file found with the name $name!")
+
     val usedUseCases = memory("usedUseCases") as List<String>? ?: emptyList()
     val fallbackCases = usedUseCases.groupingBy { it }.eachCount().filter { it.value >= fallbackLimit }.keys
     val filteredUseCases = useCases.formatToString(usedUseCases.toSet(), fallbackCases)
