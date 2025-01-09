ef78245c1e16316845a1e4c0a611f9bcb53778dc
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-01-09 10:50:32 +0100

    fix(usecases): add conditions to usecase function

--- arc-assistants/src/main/kotlin/extensions/LoadUseCases.kt
+++ arc-assistants/src/main/kotlin/extensions/LoadUseCases.kt
@@ -14,14 +14,14 @@ private val log = LoggerFactory.getLogger("UseCasesLoader")
 /**
  * Loads the use case file with the given name.
  */
-suspend fun DSLContext.useCases(name: String, fallbackLimit: Int = 2): String {
+suspend fun DSLContext.useCases(name: String, fallbackLimit: Int = 2, conditions: Set<String> = emptySet()): String {
     val requestUseCase = system("usecase", defaultValue = "").takeIf { it.isNotEmpty() }
     val useCases =
         (requestUseCase ?: local(name))?.toUseCases() ?: kotlin.error("No use case file found with the name $name!")
 
     val usedUseCases = memory("usedUseCases") as List<String>? ?: emptyList()
     val fallbackCases = usedUseCases.groupingBy { it }.eachCount().filter { it.value >= fallbackLimit }.keys
-    val filteredUseCases = useCases.formatToString(usedUseCases.toSet(), fallbackCases)
+    val filteredUseCases = useCases.formatToString(usedUseCases.toSet(), fallbackCases, conditions)
     log.info("Loaded use cases: ${useCases.map { it.id }} Fallback cases: $fallbackCases")
     return filteredUseCases
 }
