db800310af0540df08736ff7a6c41014fa909d2d
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-01-18 16:53:57 +0100

    feat(api): add binary data

--- arc-api/src/main/kotlin/Message.kt
+++ arc-api/src/main/kotlin/Message.kt
@@ -12,4 +12,18 @@ data class Message(
     val content: String,
     val format: String = "text",
     val turnId: String? = null,
+    val binaryData: List<BinaryData>? = null,
 )
+
+/**
+ * Presents binary data in a message. The data can be encoded as base64 directly in this object or
+ * this object may contain a source from where the data can be read, for example, a http address.
+ */
+@Serializable
+class BinaryData(val mimeType: String, val dataAsBase64: String? = null, val source: String? = null)
+
+/**
+ * Indicates that the binary data is being streamed to the receiver server from the client.
+ * This is used as a source in the [BinaryData] object.
+ */
+const val STREAM_SOURCE = "STREAM_SOURCE"
---
+++ arc-api/src/main/kotlin/RequestEnd.kt
@@ -0,0 +1,6 @@
+package org.eclipse.lmos.arc.api
+
+/**
+ * Indicates the end of a request.
+ */
+const val REQUEST_END = "<FIN>"
