0e10355a3cf3f16f9f51131af5d70845a58d295f
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-02-09 11:54:36 +0100

    feat: provide use cases as local variable.

--- arc-assistants/src/main/kotlin/extensions/LoadedUseCases.kt
+++ arc-assistants/src/main/kotlin/extensions/LoadedUseCases.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.eclipse.lmos.arc.assistants.support.extensions
 
 import org.eclipse.lmos.arc.assistants.support.usecases.UseCase
